c62ff4034bda9efd5c632983119c33519f9bf3a2
Route9Library <you@example.com>
2020-08-12 16:01:19 -0400

    adding

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function convertTemp(type) {
+    var docF = document.getElementById("degF");
+    var docC = document.getElementById("degC");
+    var docK = document.getElementById("degK");
+    var tempF;
+    var tempC;
+    var tempK;
+ }
+
+ docF.style.backgroundColor = '#fee';
+ docC.style.backgroundColor = '#fee';
+ docK.style.backgroundColor = '#fee';
+
+ 
